eb674b43be35ab6db7fba51bb2cc9cf3f69b1d3a
HChong <hchong7557@gmail.com>
2017-08-14 16:52:34 +0800

    first commit
    Signed-off-by: HChong <hchong7557@gmail.com>

---
+++ Example/Pods/Target Support Files/HCPods/HCPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HCPods : NSObject
+@end
+@implementation PodsDummy_HCPods
+@end
---
+++ Example/Pods/Target Support Files/Pods-HCPods_Example/Pods-HCPods_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HCPods_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HCPods_Example
+@end
